422e0d8a06998818f64e89af552f70339387889a
dbf <13336073+fullonic@users.noreply.github.com>
2020-10-18 14:48:23 +0200

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -22,7 +22,3 @@ class JobFactory(factory.django.DjangoModelFactory):
     title = factory.Sequence(lambda n: "Title %d" % n)
     description = factory.Sequence(lambda n: "Description %d" % n)
     type = "1"
-
-
-UserFactory: "factory.django.DjangoModelFactory"
-JobFactory: "factory.django.DjangoModelFactory"
